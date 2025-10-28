02da64e402a046768b29206ab63afaf0a1b81033
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-10-28 16:43:18 +0500

    Testing for insert and delete is now complete

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Tree } from "./binarysearchtree.js"
+
+const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+bst.prettyPrint();
+bst.insert(12);
+bst.prettyPrint();
+bst.deleteItem(67);
+bst.prettyPrint();
+
+const bsttwo = new Tree([10, 5]);
+bsttwo.prettyPrint();
+bsttwo.deleteItem(5);
+bsttwo.prettyPrint();
