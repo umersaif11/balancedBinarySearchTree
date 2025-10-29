f3686ef68f3887ba6d9a045c3d6b76f4e56e62ed
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-10-29 12:36:20 +0500

    If left and right childs of node is not null then push them into queue

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -117,6 +117,12 @@ class Tree {
         while(queue.length > 0) {
             let current = queue.shift();
             callback(current);
+            if(current.left != null) {
+                queue.push(current.left);
+            }
+            if(current.right != null) {
+                queue.push(current.right);
+            }
         }
     }
 }
