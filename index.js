1af920286b77e37bf2b4f4beb2b23336a54310d9
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-11-02 19:38:44 +0500

    Testing is complete and giving right results

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 import { Tree } from "./binarysearchtree.js"
 
-const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-bst.prettyPrint();
-bst.insert(12);
-bst.prettyPrint();
-bst.deleteItem(67);
-bst.prettyPrint();
-console.log(bst.find(324));
-console.log(bst.height(8));
-console.log(bst.depth(23));
+const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67]);
+
 console.log(bst.isBalanced());
 
-// const bsttwo = new Tree([10, 5]);
-// bsttwo.prettyPrint();
-// bsttwo.deleteItem(5);
-// bsttwo.prettyPrint();
+bst.prettyPrint();
+
+const levelOrderArray = [];
+bst.levelOrderForEach((node) => {
+    levelOrderArray.push(node.data);
+});
+console.log('Level Order Traversal: ', levelOrderArray.join(' --> '));
+
+const preOrderArray = [];
+bst.preOrderForEach((node) => {
+    preOrderArray.push(node.data);
+});
+console.log('Pre Order Traversal: ', preOrderArray.join(' --> '));
+
+const inOrderArray = [];
+bst.inOrderForEach((node) => {
+    inOrderArray.push(node.data);
+});
+console.log('In Order Traversal: ', inOrderArray.join(' --> '));
+
+const postOrderArray = [];
+bst.postOrderForEach((node) => {
+    postOrderArray.push(node.data);
+});
+console.log('Post Order Traversal: ', postOrderArray.join(' --> '));
+
+bst.insert(149);
+bst.insert(135);
+bst.insert(177);
+bst.insert(191);
+
+console.log(bst.isBalanced());
+
+bst.prettyPrint();
+
+bst.rebalance();
+
+console.log(bst.isBalanced());
+
+bst.prettyPrint();
+
+const levelOrderArrayTwo = [];
+bst.levelOrderForEach((node) => {
+    levelOrderArrayTwo.push(node.data);
+});
+console.log('Level Order Traversal: ', levelOrderArrayTwo.join(' --> '));
+
+const preOrderArrayTwo = [];
+bst.preOrderForEach((node) => {
+    preOrderArrayTwo.push(node.data);
+});
+console.log('Pre Order Traversal: ', preOrderArrayTwo.join(' --> '));
+
+const inOrderArrayTwo = [];
+bst.inOrderForEach((node) => {
+    inOrderArrayTwo.push(node.data);
+});
+console.log('In Order Traversal: ', inOrderArrayTwo.join(' --> '));
+
+const postOrderArrayTwo = [];
+bst.postOrderForEach((node) => {
+    postOrderArrayTwo.push(node.data);
+});
+console.log('Post Order Traversal: ', postOrderArrayTwo.join(' --> '));
